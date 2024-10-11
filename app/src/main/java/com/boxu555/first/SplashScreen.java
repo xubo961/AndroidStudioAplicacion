ba600ad00a4004d36e1b2d1d7ba863dcaf8c22e0
xubo961 <xubop961@gmail.com>
2024-10-11 13:17:36 +0200

    android studio realizado en clase y funciona de lokosss

---
+++ app/src/main/java/com/boxu555/first/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.boxu555.first;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Animation gradientColor = AnimationUtils.loadAnimation(this, R.anim.gradient_colour);
+        ImageView splashLogo = findViewById(R.id.logoSplash);
+        TextView splashAppName = findViewById(R.id.splashAppName);
+        splashLogo.startAnimation(gradientColor);
+        splashAppName.startAnimation(gradientColor);
+
+        ImageView glideBackground = findViewById(R.id.splashGlideBakcground);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1463693396721-8ca0cfa2b3b5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .centerCrop()
+                .into(glideBackground);
+    }
+}
